9d8ff778f2d9f538d636ae32bcfafcdba6d92645
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-09-24 10:13:37 -0400

    Update Inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,23 +1,55 @@
+package Assignment2;
+//package com.oops.concepts;
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * This class adds the guitar details
+ * to the inventory,search operation
+ * is performed to get the matching details
+ * a particular guitar from inventory
+ * 
+ * 
+ * 
+ * 
+ * @author Sindhuja Morampudi,Divya Kasu
+ * @version 1.0
+ * @since   09-23-2017
+ */
 public class Inventory {
-  private List guitars;
+  private List<Guitar> guitars;
 
   public Inventory() {
-    guitars = new LinkedList();
+    guitars = new ArrayList<Guitar>();
   }
 
-  public void addGuitar(String serialNumber, double price,
-                        String builder, String model,
-                        String type, String backWood, String topWood) {
-    Guitar guitar = new Guitar(serialNumber, price, builder,
-                               model, type, backWood, topWood);
+  /**
+ * adds guitar to inventory
+ * @param serialNumber
+ * @param price
+ * @param spec
+ */
+public void addGuitar(String serialNumber, double price,
+                        GuitarSpec spec) {
+    Guitar guitar = new Guitar(serialNumber, price, spec);
     guitars.add(guitar);
   }
-  public Guitar getGuitar(String serialNumber) {
-    for (Iterator i = guitars.iterator(); i.hasNext(); ) {
+
+  public List<Guitar> getAllGuitars(){
+	  
+	  return guitars;
+  }
+  
+  
+  
+  /**
+ * searches by serial number
+ * @param serialNumber
+ * @return guitar or null
+ */
+public Guitar getGuitar(String serialNumber) {
+    for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
       if (guitar.getSerialNumber().equals(serialNumber)) {
         return guitar;
@@ -25,33 +57,90 @@ public class Inventory {
     }
     return null;
   }
-  public Guitar search(Guitar searchGuitar) {
-    for (Iterator i = guitars.iterator(); i.hasNext(); ) {
+
+  /**
+ * returns list of matching guitars
+ * @param searchSpec
+ * @return matching guitars
+ */
+public List<Guitar> search(GuitarSpec searchSpec) {
+    List<Guitar> matchingGuitars = new ArrayList<Guitar>();
+    for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
       Guitar guitar = (Guitar)i.next();
-      // Ignore serial number since that's unique
-      // Ignore price since that's unique
-      String builder = searchGuitar.getBuilder();
-      if ((builder != null) && (!builder.equals("")) &&
-          (!builder.equals(guitar.getBuilder())))
-        continue;
-      String model = searchGuitar.getModel();
-      if ((model != null) && (!model.equals("")) &&
-          (!model.equals(guitar.getModel())))
-        continue;
-      String type = searchGuitar.getType();
-      if ((type != null) && (!searchGuitar.equals("")) &&
-          (!type.equals(guitar.getType())))
-        continue;
-      String backWood = searchGuitar.getBackWood();
-      if ((backWood != null) && (!backWood.equals("")) &&
-          (!backWood.equals(guitar.getBackWood())))
-        continue;
-      String topWood = searchGuitar.getTopWood();
-      if ((topWood != null) && (!topWood.equals("")) &&
-          (!topWood.equals(guitar.getTopWood())))
-        continue;
-      return guitar;
+      if (guitar.getSpec().matches(searchSpec))
+        matchingGuitars.add(guitar);
     }
-    return null;
+    return matchingGuitars;
   }
+
+  
+/**
+ * searches by price
+ * @param d
+ * @return guitar or null
+ */
+public Guitar getGuitarByPrice(double d) {
+	for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
+	      Guitar guitar = (Guitar)i.next();
+	      if (guitar.getPrice()==d) {
+	        return guitar;
+	      }
+	    }
+	    return null;
+}
+ 
+  
+/**
+ * searches with incomplete data
+ * @param guitindata
+ * 
+ */
+public List<Guitar> getGuitarWithIncompleteData(Guitar guitindata) {
+	List<Guitar> matchingGuitars = new ArrayList<Guitar>();
+	for (Iterator<Guitar> i = guitars.iterator(); i.hasNext(); ) {
+	      Guitar guitar = (Guitar)i.next();
+	      if (guitar.getSerialNumber().equals(guitindata.getSerialNumber())) {
+	    	  matchingGuitars.add(guitar);
+	    	  
+	      }
+	    	  else if(guitar.getPrice()==(guitindata.getPrice())){
+	    		  matchingGuitars.add(guitar);
+	    		 
+	    	  }
+	    	  else if(guitar.getSpec().matches(guitindata.getSpec()))
+	    	  {
+	    		  matchingGuitars.add(guitar);
+	    		  
+	    	  }
+	    	  else if(guitar.getSpec().getModel().equalsIgnoreCase(guitindata.getSpec().getModel()))
+	    	  {
+	    		  matchingGuitars.add(guitar);
+	    		  
+	    	  }
+	    	  else if(guitar.getSpec().getBackWood().equals(guitindata.getSpec().getBackWood()))
+	    	  {
+	    		  matchingGuitars.add(guitar);
+	    		  
+	    	  }
+	    	  else if(guitar.getSpec().getTopWood().equals(guitindata.getSpec().getTopWood()))
+	    	  {
+	    		  matchingGuitars.add(guitar);
+	    	  }
+	    	  else if(guitar.getSpec().getType().equals(guitindata.getSpec().getType()))
+	    	  {
+	    		  matchingGuitars.add(guitar);
+	    	  }
+	    	  else if(guitar.getSpec().getBuilder().equals(guitindata.getSpec().getBuilder()))
+	    	  {
+	    		  matchingGuitars.add(guitar);
+	    	  }
+	        
+	      
+	    }
+	    return matchingGuitars;
+}
+
+
+
+ 
 }
