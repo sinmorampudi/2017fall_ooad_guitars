bf233492e20cca5ce989eb6f9307ced21631c22f
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-10-04 23:18:32 -0400

    Update GuitarSpec.java

--- GuitarSpec.java
+++ GuitarSpec.java
@@ -1,10 +1,15 @@
-package Assignment2;
-
-
 
 /**
+ * Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 2
  * 
- * getters and setters for guitar spec properties
+ * Date: 2017-10-04
+ */
+package edu.fsu.Y201709Ooad.guitars_enums;
+/**
+ * 
+ * getters and setters for guitar specification properties
  * 
  * @author Divya Kasu,Sindhuja Morampudi
  * @version 1.0
@@ -19,90 +24,154 @@ public class GuitarSpec {
   private Wood backWood;
   private Wood topWood;
   
-  //parameterized constructor
+  
 
-  public GuitarSpec(Builder builder, String model, Type type,
+  /**
+ * Parameterized constructor for guitar specification properties
+ * @param builder,this is the builder name for guitar
+ * @param model,this is the model name  of the guitar
+ * @param type,this is the type of the guitar
+ * @param numStrings,this is the number of the strings for the guitqr
+ * @param backWood,this is type of backWood for guitar
+ * @param topWood,this is the type of topWood for guitar
+ */
+public GuitarSpec(Builder builder, String model, Type type,
                     int numStrings, Wood backWood, Wood topWood) {
-	    setModel(model);
-		setBuilder(builder);
-		setType(type);
-		setBackWood(backWood);
-		setTopWood(topWood);
+
+	  this.model=model;
+	  this.builder=builder;
+	  this.type=type;
+	  this.backWood=backWood;
+	  this.topWood=topWood;
+	  this.numStrings=numStrings;
+
   }
 
-  public GuitarSpec(Builder builder) {
+    
+  /**
+ * Parameterized constructor to set builder value
+ * @param builder,this is the builder name for guitar
+ */
+public GuitarSpec(Builder builder) {
 		
-		setBuilder(builder);
+	setBuilder(builder);
 		
 }
 
-  public GuitarSpec(Type type) {
+ /**
+ * Parameterized constructor to set type value
+ * @param type,this is the type of the guitar
+ */
+public GuitarSpec(Type type) {
 		
 	  setType(type);
 		
 }
   
   
-  public GuitarSpec() {
+ /**
+ * Default constructor
+ */
+public GuitarSpec() {
 	// TODO Auto-generated constructor stub
 }
 
 
 
+/**
+ * Getter for the Builder value
+ * @return builder,this is name of the builder of the guitar
+ */
 public Builder getBuilder() {
 	return builder;
 }
 
 
 
+/**
+ * Setter for the Builder value
+ * @param builder,this is name of the builder of the guitar
+ */
 public void setBuilder(Builder builder) {
 	this.builder = builder;
 }
 
 
 
+/**
+ * Getter for the model name of the guitar
+ * @return model,this is the name of the model name of the guitar
+ */
 public String getModel() {
 	return model;
 }
 
 
 
+/**
+ * Setter for the model name of the guitar
+ * @param model,this is the name of the model name of the guitar
+ */
 public void setModel(String model) {
 	this.model = model;
 }
 
 
 
+/**
+ * Getter for the type of the guitar
+ * @return type,this is the type of the guitar
+ */
 public Type getType() {
 	return type;
 }
 
 
 
+/**
+ * Setter for the type of the guitar
+ * @param type,this is the type of the guitar
+ */
 public void setType(Type type) {
 	this.type = type;
 }
 
 
 
+/**
+ * Getter for the type of backwood for guitar
+ * @return backWood,this is type of backwood for guitar
+ */
 public Wood getBackWood() {
 	return backWood;
 }
 
 
 
+/**
+ * Setter for the type of backwood for guitar
+ * @param backWood,this is type of backwood for guitar
+ */
 public void setBackWood(Wood backWood) {
 	this.backWood = backWood;
 }
 
 
 
+/**
+ * Getter for the type of topWood for guitar
+ * @return topWood,this is type of topWood for guitar
+ */
 public Wood getTopWood() {
 	return topWood;
 }
 
 
 
+/**
+ * Setter for the type of topWood for guitar
+ * @param topWood,this is type of topWood for guitar
+ */
 public void setTopWood(Wood topWood) {
 	this.topWood = topWood;
 }
@@ -110,16 +179,16 @@ public void setTopWood(Wood topWood) {
 
 
 /**
- * compare matchSpec properties with enum vallues
- * @param matchSpec
- * @return true/false
+ * Compares every guitar property with the input guitar properties
+ * @param matchSpec,this contains guitar specification properties of I/P guitar
+ * @return true/false, returns true if all the properties are matched 
+ * and returns false if at least one property is not matched or null 
  */
 public boolean matches(GuitarSpec matchSpec) {
     if (builder != matchSpec.builder)
-      return false;
+    	return false;
    // if ((model != null) && (!model.equals("")) &&
-    		if ((model != null) && (model.equals("")) &&		
-        (!model.toLowerCase().equals(matchSpec.model.toLowerCase())))
+    if (model!=null && !model.equals("") && !model.toLowerCase().equals(matchSpec.model.toLowerCase()))
       return false;
     if (type != matchSpec.type)
       return false;
