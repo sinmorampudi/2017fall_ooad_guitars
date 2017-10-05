2600d1b5c0d7471db10039ab65838cddc449be8a
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-10-04 23:17:18 -0400

    Update Guitar.java

--- Guitar.java
+++ Guitar.java
@@ -1,4 +1,11 @@
-package Assignment2;
+/**
+ * Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 2
+ * 
+ * Date: 2017-10-04
+ */
+package edu.fsu.Y201709Ooad.guitars_enums;
 /**
  * 
  * Guitar properties and getter and setter methods are 
@@ -10,39 +17,64 @@ package Assignment2;
  * @version 1.0
  * @since   09-23-2017
  */
+
+
+
 public class Guitar {
 
   private String serialNumber;
   private double price;
   GuitarSpec spec;
 
-//Parameterized constructor
-  public Guitar(String serialNumber, double price, GuitarSpec spec) {
+  /**
+ * Parameterized constructor
+ * @param serialNumber,this is the serial number for guitar given by sales company
+ * @param price,this is the price of the guitar given by sales company
+ * @param spec,spec contains all the specifications of guitar other than serialNumber & price
+ */
+public Guitar(String serialNumber, double price, GuitarSpec spec) {
     this.serialNumber = serialNumber;
     this.price = price;
     this.spec = spec;
   }
   
-//getters and setters for guitar properties
 
-  public String getSerialNumber() {
+ 
+ /**
+ * Getter for the serial number of the Guitar
+ * @return serialNumber,this is serial number given by sales company
+ */
+public String getSerialNumber() {
     return serialNumber;
   }
 
-  public double getPrice() {
+  
+ /**
+ * Getter for price of the guitar
+ * @return price ,this is price of the Guitar given by sales company
+ */
+public double getPrice() {
     return price;
   }
 
-  public void setPrice(float newPrice) {
+
+  /**
+ * Setter for the price of the guitar
+ * @param price ,this is price of the Guitar given by sales company
+ */
+public void setPrice(float newPrice) {
     this.price = newPrice;
   }
-
+  
+ 
+/**
+ * Getter for specifications of the guitar
+ * @return spec ,this is specification of the Guitar 
+ */
   public GuitarSpec getSpec() {
     return spec;
   }
   
-  public String toString(){
-      return serialNumber+"         : "+price;
-  }
+ 
 
 }
