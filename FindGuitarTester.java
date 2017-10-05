40b675b40a00ce966746da3864e889cdaf1faf7f
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-10-04 23:16:31 -0400

    Update FindGuitarTester.java

--- FindGuitarTester.java
+++ FindGuitarTester.java
@@ -1,5 +1,11 @@
-
-package Assignment2;
+/**
+ * Class: Object-Oriented Design and Analysis
+ * Professor: Orlando Montalvo
+ * Assignment: HW 2
+ * 
+ * Date: 2017-10-04
+ */
+package edu.fsu.Y201709Ooad.guitars_enums;
 
 
 import java.util.Iterator;
@@ -7,7 +13,7 @@ import java.util.List;
 
 
 /**
- * This FindGuitarTester class is used to test the functionalities 
+ * Used to test the functionalities 
  * like adding guitar,getting price,searching guitar ect
  * 
  * 
@@ -19,26 +25,24 @@ import java.util.List;
  */
 
 public class FindGuitarTester {
-	
- /**
+/**
  * 
- * prints the data for complete and incomplete search
+ * Prints the data for complete and incomplete search
  * 
- */
-	
-	 static GuitarSpec whatErinLikes;
+ */	 	
   public static void main(String[] args) {
     // Set up Rick's guitar inventory
+	  
     Inventory inventory = new Inventory();
     initializeInventory(inventory);
     
-    //input for complete search
-     whatErinLikes = 
-      new GuitarSpec(Builder.FENDER, "Stratocastor", 
-                     Type.ELECTRIC, 6, Wood.ALDER, Wood.ALDER);
+    //Input for complete search
+    GuitarSpec whatErinLikes = 
+      new GuitarSpec(Builder.MARTIN, "OM-28", Type.ACOUSTIC, 6,
+              Wood.BRAZILIAN_ROSSWOOD, Wood.ADIRONDACK);
     List<Guitar> matchingGuitars = inventory.search(whatErinLikes);
     if (!matchingGuitars.isEmpty()) {
-      System.out.println("Erin, you might like these guitars:");
+      System.out.println("!!!!Erin, you might like these guitars:");
       for (Iterator<Guitar> i = matchingGuitars.iterator(); i.hasNext(); ) {
         Guitar guitar = i.next();
         GuitarSpec spec = guitar.getSpec();
@@ -53,22 +57,34 @@ public class FindGuitarTester {
       System.out.println("Sorry, Erin, we have nothing for you.");
     }
     
-        //input for search based on serial number
+        //Input for search based on serial number
     Guitar guitar = inventory.getGuitar("827655001");
     if(guitar!=null && guitar.getSerialNumber().equals("82765501")){
 		System.out.println("Yes I got the Guitar, was the one I am looking for !!!! \n");
 	}else{
-		System.out.println("Requested Guitar is not available in the List");
+		System.out.println(" Requested Guitar with this serial number is not available in the List");
 	}
         
-        //input for search based on price
-    Guitar guitarprice = inventory.getGuitarByPrice(3999.95d);
-    if(guitarprice!=null && guitarprice.getPrice()==3999.95d){
-		System.out.println("Yes I got the Guitar, was the one I am looking for !!!! \n" +guitarprice.getSerialNumber());
+        //Input for search based on price
+    List<Guitar> guitarprice = inventory.getGuitarByPrice(3000.00d,6000.00d);
+    if(guitarprice!=null ){
+    	System.out.println("Yes we have "+" "+guitarprice.size()+""+"matching guitars with in this price range " );
+    	for (Iterator<Guitar> i = guitarprice.iterator(); i.hasNext(); )
+    	{
+    		Guitar guitar1 = i.next();
+        GuitarSpec spec = guitar1.getSpec();
+         System.out.println("  We have a " +
+           spec.getBuilder() + " " + spec.getModel() + " " +
+           spec.getType() + " guitar:\n     " +
+           spec.getBackWood() + " back and sides,\n     " +
+           spec.getTopWood() + " top.\n  You can have it for only $" +
+           guitar1.getPrice() + "!\n  ----");
+		
+    	}
 	}else{
 		System.out.println("Requested Guitar is not available in the List");
 	}
-       //input for incomplete data search
+       //Only one input value to search for Guitar
     Guitar incompletedata=new Guitar("V95693",0,new GuitarSpec());
     List<Guitar> matchingGuitars1 = inventory.getGuitarWithIncompleteData(incompletedata);
     if(matchingGuitars1!=null && matchingGuitars1.size()>0)
@@ -90,12 +106,12 @@ public class FindGuitarTester {
 
   /**
  * Guitars are added to inventory
- * @param inventory
+ * @param inventory,this is an inventory object
  */
 private static void initializeInventory(Inventory inventory) {
     inventory.addGuitar("11277", 3999.95, 
       new GuitarSpec(Builder.COLLINGS, "CJ", Type.ACOUSTIC, 6,
-                     Wood.INDIAN_ROSSWOOD, Wood.SITKA));
+                     Wood.INDIAN_ROSEWOOD, Wood.SITKA));
     inventory.addGuitar("V95693", 1499.95, 
       new GuitarSpec(Builder.FENDER, "Stratocastor", Type.ELECTRIC, 6,
                      Wood.ALDER, Wood.ALDER));
@@ -123,9 +139,9 @@ private static void initializeInventory(Inventory inventory) {
     inventory.addGuitar("566-62", 8999.95, 
       new GuitarSpec(Builder.RYAN, "Cathedral", Type.ACOUSTIC, 12,
                      Wood.COCOBOLO, Wood.CEDER));
-    inventory.addGuitar("6 29584", 2100.95, 
-      new GuitarSpec(Builder.PRS, "Dave Navarro Signature", Type.ELECTRIC, 
-                     6, Wood.MAHOGANY, Wood.MAPPLE));
+   inventory.addGuitar("6 29584", 2100.95, 
+     new GuitarSpec(Builder.PRS, "Dave Navarro Signature", Type.ELECTRIC, 
+                    6, Wood.MAHOGANY, Wood.MAPPLE));
     
   }
   
